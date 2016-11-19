3b75b2957210d84aa35ec53873a155f4c77c4a27
Junaid Anwar <junaid4567@gmail.com>
2016-11-19 21:57:24 +0500

    added bootstrap, body-parser and  on the right path with nunjucks

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var express 	= require('express');
-var nunjucks 	= require('nunjucks');
+var nunjucks    = require('nunjucks');
+var bodyParser 	= require('body-parser');
 var app     	= express();
 
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 
 nunjucks.configure('views', {
@@ -9,48 +12,39 @@ nunjucks.configure('views', {
     express: app
 });
 
-// app.get('/', function(req, res) {
-//     res.render('index.html');
-// });
-
 
 app.get('/', function (req, res) {
     // res.json({"status": 1, "reason" : "Hello there"});
     res.render('index.html', {
-
-    	title : 'My First Nunjucks Page',
-	    items : [
-	      { name : 'item #1' },
-	      { name : 'item #2' },
-	      { name : 'item #3' },
-	      { name : 'item #4' },
-	    ],
-
-	    divideTitle: function(){
-	    	var titleArr = this.title.split(" ");
-	    	alert(titleArr);
-	    }
-
+    	title : 'Welcome to my TODO app',
     })
 });
 
 app.post('/create', function (req, res) {
-    res.json({"status": 1, "reason" : "Hello there"});
+    console.log(req.body);
+    // res.json(req.body);
+    res.render('create-todo.html', {
+        title           : req.body.name,
+        quoteContent    : req.body.quote
+    });
     // res.send('create-todo.html');
 });
 
-app.post('/update/:id', function (req, res) {
-    res.json({"status": 1, "reason" : "Now updating " + req.params.id});
-});
 
-app.post('/delete/:id', function (req, res) {
-    res.json({"status": 1, "reason" : "Now deleting " + req.params.id});
-});
 
-app.get('/test/:id', function (req, res) {
-    res.json({"status": 1, "reason" : "Now Testing " + req.params.id});
-});
 
-app.listen((process.env.PORT || 3000), function () {
-    console.log('Here you go, Open localhost:3000 and see you app running');
+// app.post('/update/:id', function (req, res) {
+//     res.json({"status": 1, "reason" : "Now updating " + req.params.id});
+// });
+
+// app.post('/delete/:id', function (req, res) {
+//     res.json({"status": 1, "reason" : "Now deleting " + req.params.id});
+// });
+
+// app.get('/test/:id', function (req, res) {
+//     res.json({"status": 1, "reason" : "Now Testing " + req.params.id});
+// });
+
+app.listen((process.env.PORT || 3003), function () {
+    console.log('Here you go, Open localhost:3003 and see you app running');
 })
