2311e373163469ea595efac56db26e861d0b9fe6
Junaid Anwar <junaid4567@gmail.com>
2016-11-19 22:38:08 +0500

    dummy data added to pages and dummy task submission emulated

--- index.js
+++ index.js
@@ -17,6 +17,12 @@ app.get('/', function (req, res) {
     // res.json({"status": 1, "reason" : "Hello there"});
     res.render('index.html', {
     	title : 'Welcome to my TODO app',
+        tasks : [
+          { taskName : 'Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.' },
+          { taskName : 'Lorem ipsum dolor sit amet, consectetuer dolore psum dolor sit.' },
+          { taskName : 'Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.' },
+          { taskName : 'consectetuer sit amet ipsum dolor, consectetuer ipsum consectetuer sit amet dolore.' },
+        ]
     })
 });
 
@@ -24,8 +30,15 @@ app.post('/create', function (req, res) {
     console.log(req.body);
     // res.json(req.body);
     res.render('create-todo.html', {
-        title           : req.body.name,
-        quoteContent    : req.body.quote
+        title    : "Task submitted",
+        taskName : req.body.task,
+        tasks    : [
+          { taskName : 'Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.' },
+          { taskName : 'Lorem ipsum dolor sit amet, consectetuer dolore psum dolor sit.' },
+          { taskName : 'Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.' },
+          { taskName : 'consectetuer sit amet ipsum dolor, consectetuer ipsum consectetuer sit amet dolore.' },
+          { taskName : req.body.task}
+        ]
     });
     // res.send('create-todo.html');
 });
